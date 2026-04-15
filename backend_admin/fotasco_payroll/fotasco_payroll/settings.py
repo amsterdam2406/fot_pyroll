d1d13b3eba94aff5a27c11624997b63b74767f39
Amsat Lolade <amsatlolade@gmail.com>
2026-04-15 18:37:04 +0100

    Remove dashboardstats.js reference and update settings

--- backend_admin/fotasco_payroll/fotasco_payroll/settings.py
+++ backend_admin/fotasco_payroll/fotasco_payroll/settings.py
@@ -49,15 +49,20 @@ STATICFILES_DIRS = [
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 
-ALLOWED_HOSTS = ['fot-pyroll.onrender.com', 'fotasco-payroll.onrender.com', 'dpg-d7en59hf9bms739ngbpg-a', '127.0.0.1']
+ALLOWED_HOSTS = ['fot-pyroll.onrender.com', 
+                'fotasco-payroll.onrender.com',
+                'localhost',
+                '127.0.0.1'
+                ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -81,14 +86,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  
-
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  
 ]
 
 ROOT_URLCONF = 'fotasco_payroll.urls'
